e6779dd10f87cfcb003508c5cfee2a37abc07e6e
Nishant <nishantsingh2jan1998@gmail.com>
2021-02-22 20:22:25 +0530

    Video call

---
+++ app/src/main/java/com/example/bcall/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.bcall;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+      //  new Handler().postDelayed(new Runnable() {
+                new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    startActivity(new Intent(MainActivity.this, Dashboard.class));
+                }
+            },2000);
+        }
+    }
